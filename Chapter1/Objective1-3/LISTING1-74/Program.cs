caa8231e2a4a38f3a982170ba431a384dabaebf6
iphayao <phayaob@gmail.com>
2018-05-27 19:13:09 +0700

    Add missed Listing1-74

---
+++ Chapter1/Objective1-3/LISTING1-74/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chapter1
+{
+    /// <summary>
+    /// LISTING 1-74 goto statement with label
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            if (x == 3) goto customerLabel;
+
+            Console.WriteLine("Not display");
+
+            customerLabel:
+            Console.WriteLine(x);
+        }
+    }
+}
